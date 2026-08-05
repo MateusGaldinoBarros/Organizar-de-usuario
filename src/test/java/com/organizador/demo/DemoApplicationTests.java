ca3c76111a29656df7cd1ef958e025501d49f371
Mateus Galdino Barros <edwardaleatorio@gmail.com>
2026-08-05 11:16:31 -0300

    primeiro git

---
+++ src/test/java/com/organizador/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizador.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
